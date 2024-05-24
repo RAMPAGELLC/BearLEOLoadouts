ae517e714f276ad587b99294a10274219128bc7b
jake <53842001+vq9o@users.noreply.github.com>
2024-05-24 11:03:08 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,5 +1,19 @@
 -- Copyright (c) 2024 RAMPAGE Interactive. All rights reserved.
 
+function tablefind(t, v)
+	for _, a in pairs(t) do
+		if a == v then
+			return a
+		end
+	end
+
+	return nil
+end
+
 AddEventHandler("bearduty_rich:DUTY_STATUS_CHANGE", function(source, isOnDuty, trackData)
+    if not tablefind(AllowedDepartments, trackData.Department) then
+        return
+    end
+
     TriggerClientEvent('BearLEOLoadouts:OnEvent', source, isOnDuty)
 end)
