69fa75ff4a837b0ab24fd0db8e87287afc6907ae
jake <53842001+vq9o@users.noreply.github.com>
2024-05-24 11:00:53 -0700

    Create client.lua

---
+++ client.lua
@@ -0,0 +1,26 @@
+-- Copyright (c) 2024 RAMPAGE Interactive. All rights reserved.
+
+function giveWeapon(hash)
+	GiveWeaponToPed(GetPlayerPed(-1), GetHashKey(hash), 999, false, false)
+end
+
+RegisterNetEvent('BearLEOLoadouts:OnEvent')
+AddEventHandler('BearLEOLoadouts:OnEvent', function(isOnDuty)
+    if isOnDuty then
+        giveWeapon("weapon_nightstick")
+        giveWeapon("weapon_stungun")
+        giveWeapon("weapon_flashlight")
+        giveWeapon("weapon_combatpistol")
+        GiveWeaponComponentToPed(GetPlayerPed(-1), 1593441988, 0x359B7AAE)
+        giveWeapon("weapon_carbinerifle")
+        GiveWeaponComponentToPed(GetPlayerPed(-1), -2084633992, 0x7BC4CDDC)
+        GiveWeaponComponentToPed(GetPlayerPed(-1), -2084633992, 0xC164F53)
+        GiveWeaponComponentToPed(GetPlayerPed(-1), -2084633992, 0xA0D89C42)
+        giveWeapon("weapon_pumpshotgun")
+        GiveWeaponComponentToPed(GetPlayerPed(-1), 487013001, 0x7BC4CDDC)
+        SetPedArmour(GetPlayerPed(-1), 100)
+    else
+        SetPedArmour(GetPlayerPed(-1), 0)
+        RemoveAllPedWeapons(GetPlayerPed(-1), true);
+    end
+end)
